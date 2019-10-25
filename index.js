94cf139a36769fc06524bea57eb19dcff182ad85
Michael <michael@malinov.com>
2019-10-24 22:19:27 -0700

    _

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var net = require("net");
+var SortedArray = require("sorted-array");
+
+process.stdout.write("starting\n");
+
+var ports = new SortedArray([]);
 
 var server = net.createServer(function(socket) {
+  process.stdout.write("[Server] Established matchmaking connection\n");
 	socket.write("CanvOS 2.1 setup\r\n");
 	socket.write("Use port ");
-
+	var port;
+	while(1) {
+		port = Math.floor(Math.random()*40069) + 2000;
+	  process.stdout.write("[Matchmaking] Trying port ");
+		process.stdout.write(port.toString());
+	  process.stdout.write("\n");
+		if (ports.search(port) === -1) break;
+	}
+  socket.write(port.toString());
+	process.stdout.write("[Matchmaking] Using port ");
+	process.stdout.write(port.toString());
+	process.stdout.write("\n");
+	socket.write("\r\n");
+	socket.destroy();
+	process.stdout.write("[Matchmaking] Connection ended\n");
 });
 
-server.listen(3000, "10.166.165.192");
+server.listen(42069);
