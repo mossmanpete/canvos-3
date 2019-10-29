9741510884a2c2a304c2fc7a07c4e839e02d18f6
Michael <michael@malinov.com>
2019-10-29 14:51:51 -0700

    _

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var server = net.createServer(async function (socket) {
 	socket.write("CanvOS 2.1 booting up...");
 	var port, i = 0;
 	for (; i < 100000; i++) {
-		if (i === 10000) socket.write("\r\nSearching for an available port");
+		if (i === 10000) socket.write("\nSearching for an available port");
 		else if (i > 10000 & i % 10000 === 0) socket.write(".");
 		if (i % 1000 === 0) await pause();
 		port = Math.floor(Math.random()*40000) + 2069;
@@ -24,16 +24,16 @@ var server = net.createServer(async function (socket) {
 	}
 	if (i >= 49999) {
 			process.stdout.write("[Matchmaking] Failed to locate port\n");
-			socket.write("\r\nServer is full, Try again later\r\n");
+			socket.write("\nServer is full, Try again later\n");
 			socket.destroy();
 			process.stdout.write("[Matchmaking] Connection ended\n");
 	} else {
 		process.stdout.write("[Matchmaking] Using port ");
 		process.stdout.write(port.toString());
 		process.stdout.write("\n");
-		socket.write("\r\nConnect to port ");
+		socket.write("\nConnect to port ");
 	  socket.write(port.toString());
-		socket.write("\r\n");
+		socket.write("\n");
 		createServer(port);
 		process.stdout.write("[Server] Starting OS server on port ");
 		process.stdout.write(port.toString());
@@ -51,36 +51,48 @@ function createServer(port) {
 	ports.insert(port);
 	var serv = net.createServer(async function (socket) {;
 		socket.on("data", (data) => {
-			serv._readfunc(data.toString());
-			serv._readfunc = (data) => {};
+			serv._stringBuffer += data.toString();
+			serv._readfunc();
+			serv._readfunc = () => {};
 		});
 		process.stdout.write("[Server] Established OS connection on port ");
 		process.stdout.write(port.toString());
 		process.stdout.write("\n");
-		socket.write("CanvOS 2.1 OS\r\n");
+		socket.write("CanvOS 2.1 OS\n");
 		socket.write("Type \"nc\" for verification: ");
-		var data = await serv._readString();
-		socket.write("Recieved data " + data + "\r\n");
+		var data = await serv._gets();
+		socket.write("Recieved data\n");
 
 	});
-	serv._read = () => {
+	serv._stringBuffer = "",
+	serv._getc = function(wait = true) {
 		return new Promise(yey => {
-			serv._readfunc = yey;
+			if (wait && serv._stringBuffer.length === 0) {
+				// wait for data
+				serv._readfunc = () => {
+					var char = serv._stringBuffer[0];
+					serv._stringBuffer = serv._stringBuffer.slice(1, serv._stringBuffer.length);
+					yey(char);
+				}
+			} else {
+				// return char
+				var char = serv._stringBuffer[0];
+				serv._stringBuffer = serv._stringBuffer.slice(1, serv._stringBuffer.length);
+				yey(char);
+			}
 		});
 	}
-	serv._stringBuffer = "";
-	serv._readString = async function() {
+	serv._gets = async function(wait = true) {
+		var buf = "";
 		while (1) {
-			serv._stringBuffer += await serv._read();
-			var index = serv._stringBuffer.indexOf("\n");
-			if (index !== -1) {
-				var buf = serv._stringBuffer.split("\n")[0];
-				serv._stringBuffer = serv._stringBuffer.slice(index + 1, serv._stringBuffer.length);
-				return buf;
-			}
+			var char = await serv._getc(wait);
+			buf += char;
+			if (!wait && char === "") return buf;
+			if (char === "\n") return buf;
+			await pause();
 		}
 	}
-	serv._readfunc = (data) => {};
+	serv._readfunc = () => {};
 	serv.listen(port);
   servers[port.toString()] = {server: serv, timeout: setTimeout(() => {
 		serv.close(() => {
